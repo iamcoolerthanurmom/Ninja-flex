c80f25fdd97554ddee65682fe0cc0833cf97e322
iamcoolerthanurmom <75360513+iamcoolerthanurmom@users.noreply.github.com>
2021-02-24 11:25:42 +0100

    -

---
+++ Ninja flex/Assets/Scripts/instakill.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class instakill : MonoBehaviour
+{
+    public Transform groundCheck;
+    public float checkRadius;
+    public LayerMask whatIsGround;
+    private bool isGrounded;
+
+    public string scene;
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, whatIsGround);
+
+        if (isGrounded == true)
+        {
+            SceneManager.LoadScene(scene);
+        }
+    }
+}
---
+++ Ninja flex/Assets/Scripts/projectille.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class projectille : MonoBehaviour
+{
+    public float speed;
+
+    private Transform player;
+    private Vector2 target;
+
+    bool attack = false;
+
+    public int atk;
+    public LayerMask enemyLayers;
+    public Transform Attackpointenemy;
+    public float attackRange = 2f;
+
+    public enemy enmy;
+
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+
+        target = new Vector2(player.position.x, player.position.y);
+    }
+
+
+    void Update()
+    {
+        enmy = FindObjectOfType<enemy>();
+
+
+        if (enmy.cuurentHealth > 0)
+        {
+
+            transform.position = Vector2.MoveTowards(transform.position, target, speed * Time.deltaTime);
+
+
+            if (transform.position.x == target.x && transform.position.y == target.y)
+            {
+
+                Attack();
+                DestroyProjectile();
+            }
+        }
+
+
+    }
+
+
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            DestroyProjectile();
+
+            attack = true;
+
+            Attack();
+
+        }
+        else if (other.CompareTag("Ground"))
+        {
+            DestroyProjectile();
+        }
+    }
+
+ 
+
+
+
+
+
+    void DestroyProjectile()
+    {
+        Destroy(gameObject);
+    }
+
+    void Attack()
+    {
+        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(Attackpointenemy.position, attackRange, enemyLayers);
+
+        foreach (Collider2D enemy in hitEnemies)
+        {
+            enemy.GetComponent<enemyplayer>().TakeDamage(atk);
+        }
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        if (Attackpointenemy == null)
+            return;
+
+        Gizmos.DrawWireSphere(Attackpointenemy.position, attackRange);
+    }
+}
