a08198947e9bf3603f3a6e3f19c3ee9b86f88c48
iamcoolerthanurmom <75360513+iamcoolerthanurmom@users.noreply.github.com>
2021-03-03 14:09:55 +0100

    -

--- Ninja flex/Assets/Scripts/Movement.cs
+++ Ninja flex/Assets/Scripts/Movement.cs
@@ -139,33 +139,4 @@ public class Movement : MonoBehaviour
         rb.gravityScale = garvity;
     }
 
-
-    /*
-    public float moveSpeed = 500f;
-    public bool isGrounded = false;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        Jump();
-        Vector3 movemment = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
-        transform.position += movemment * Time.deltaTime * moveSpeed;
-    }
-
-    void Jump()
-    {
-        if (Input.GetButtonDown("Jump") && isGrounded == true)
-        {
-
-
-            gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, 5f), ForceMode2D.Impulse);
-        }
-    }
-    */
 }
