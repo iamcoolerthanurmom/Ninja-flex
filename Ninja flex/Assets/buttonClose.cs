c47539cfe7a299670f5a82a53b12aefe678eee7b
iamcoolerthanurmom <75360513+iamcoolerthanurmom@users.noreply.github.com>
2021-03-03 10:44:35 +0100

    close function

---
+++ Ninja flex/Assets/buttonClose.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class buttonClose : MonoBehaviour
+{
+
+    public Button btnclose;
+
+
+    void Start()
+    {
+        Button btn = btnclose.GetComponent<Button>();
+        btn.onClick.AddListener(QuitGame);
+    }
+    void QuitGame()
+    {
+        Application.Quit();
+        Debug.Log("Game is exiting");
+    }
+}
