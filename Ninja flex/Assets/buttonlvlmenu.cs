81c3dca2f3645e2b8c3ced72f98dc63b911ac716
iamcoolerthanurmom <75360513+iamcoolerthanurmom@users.noreply.github.com>
2021-03-03 11:15:32 +0100

    menu update

---
+++ Ninja flex/Assets/buttonlvlmenu.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class buttonlvlmenu : MonoBehaviour
+{
+    public Button btnplay;
+
+
+    void Start()
+    {
+        Button btn = btnplay.GetComponent<Button>();
+        btn.onClick.AddListener(TaskOnClick);
+    }
+
+    void TaskOnClick()
+    {
+        SceneManager.LoadScene("Levelmenu");
+    }
+}
